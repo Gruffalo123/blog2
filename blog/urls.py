f41f263036dae6a8fc0fa3ea01da66747a0464d7
unknown <houxinxinbox@163.com>
2019-04-20 22:43:00 +0800

    fenye2

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ import blog.views as bv
 urlpatterns = [
     path('hello_world/',bv.hello_world),
     path('content/',bv.article_content),
-    path('index/',bv.get_index_page),
+    path('index',bv.get_index_page),
     # path('detail',bv.get_detail_page)
-    path('detail/<int:article_id>/',bv.get_detail_page)
+    path('detail/<int:article_id>',bv.get_detail_page)
 ]
